d68c9072c0b5b4136bbdda50a227f9f3e6ee2f14
Salocin.TEN <salocin.ten@gmail.com>
2016-02-21 23:06:15 +0800

    Enhance calculation based on table.
    WOOHOOO! THE END IS IN SIGHT!!!

--- dist/js/engine1.js
+++ dist/js/engine1.js
@@ -9,13 +9,28 @@ angular.module('PFDApp', []).controller('PFDController', ['$scope', function($sc
 		// $scope.counter++;
 		$scope.expenseItem = "";
 		$scope.expenseCash = "";
+	    // $scope.totalSpent = 0;
+	    // for(var i = 0; i < $scope.expenses.length; i++){
+	    //     var addTuple = $scope.expenses[i];
+	    //     $scope.totalSpent += addTuple.cashSpent;
+	    // }
+		// $scope.totalSpent += $scope.expenseItem; 
 	}
 
 	$scope.delRow = function(index) {
 		$scope.expenses.splice(index, 1);
-		// if ($scope.counter > 1) {
-		// 	$scope.expenses.pop($scope.counter);
-		// 	$scope.counter--;			
-		// }
+	    // for(var i = 0; i < $scope.expenses.length; i++){
+	        // var tuple = $scope.expenses[index];
+	        // $scope.totalSpent -= tuple.cashSpent;
+	    // }
+	}
+
+	$scope.totalSpent = function(){
+	    var total = 0;
+	    for(var i = 0; i < $scope.expenses.length; i++){
+	        var product = $scope.expenses[i];
+	        total += product.cashSpent;
+	    }
+	    return total;
 	}
 }]);
