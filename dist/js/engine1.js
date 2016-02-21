51ac0885e1dcee2f35cee3c7328f95fddbb2bb2e
Salocin.TEN <salocin.ten@gmail.com>
2016-02-21 17:14:49 +0800

    Finally getting pricing list correct

--- dist/js/engine1.js
+++ dist/js/engine1.js
@@ -1,17 +1,18 @@
 angular.module('PFDApp', []).controller('PFDController', ['$scope', function($scope){
-	var expenses = $scope.expenses = [{item: '', cashSpent: ''}];
-
-	$scope.item = '';
-
-	$scope.cashSpent = 0;
+	$scope.expenses = [];
 
 	$scope.addRow = function() {
-		expenses.push({item, cashSpent});
+		$scope.expenses.push({
+			item: $scope.expenseItem,
+			cashSpent: $scope.expenseCash
+		});
 		// $scope.counter++;
+		$scope.expenseItem = "";
+		$scope.expenseCash = "";
 	}
 
-	$scope.delRow = function() {
-		expenses.splice(index, 1);
+	$scope.delRow = function(index) {
+		$scope.expenses.splice(index, 1);
 		// if ($scope.counter > 1) {
 		// 	$scope.expenses.pop($scope.counter);
 		// 	$scope.counter--;			
